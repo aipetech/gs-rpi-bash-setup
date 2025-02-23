52394d413b7911bf5b767211759124c3c916be88
Patrik Ekström <aipe.silte@gmail.com>
2025-02-23 01:08:03 +0100

    Add alias for LAN scanning and enhance installation script

--- .bash_aliases
+++ .bash_aliases
@@ -1,3 +1,3 @@
 alias ll='ls -lha'
 alias l='ls -CF'
-
+alias lan-scan="nmap $(ip addr show $(ip route | awk '/default/ {print $5}') | awk '/inet / {print $2}' | awk -F'/' '{print $1}' | awk -F'.' '{print $1"."$2"."$3".-"}')"
--- standard-install.sh
+++ standard-install.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 # [ $(pwd) == $(echo $HOME) ] && echo "ok" || echo "$(pwd) is not $HOME"
-if [ $(pwd) != $(echo $HOME) ] ; then
+echo "Checking if .bash_aliases exists in $HOME"
+if [ -f ~/.bash_aliases ] ; then
+	echo ".bash_aliases exists in $HOME"
+else
+	echo ".bash_aliases does not exist in $HOME, fixing..."
 	cp .bash_aliases ~/
+	source ~/.bash_aliases
 fi
-
-source .bash_aliases
 
 # comment this out. Use raspi-config to enable pigpiod instead
 # sudo chmod 0755 pigpiod.service && sudo chown root pigpiod.service && sudo cp pigpiod.service /etc/systemd/system/
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
+echo "Installing tmux, mc, neovim, nmap, arp-scan"
 sudo apt-get -y install tmux mc neovim nmap arp-scan
+sudo apt autoremove -y
 
+echo "Installing pigpio"
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
 
 # check if ssh key exists, else create
 # add steps for docker installation
 
+echo "Setting default GIT repo name to main"
 git config --global init.defaultBranch main
